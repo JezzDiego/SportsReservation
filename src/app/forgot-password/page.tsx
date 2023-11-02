36a01e01c9d4a6d6523cf915719fbf4b0b8af5fc
JezzDiego <oliveirajesse50@gmail.com>
2023-11-02 19:47:48 -0300

    Refactor forgot password and signin pages

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -1,48 +1,5 @@
-import AccountCard from "@/components/custom/AccountCard";
-import { Button } from "@/components/ui/button";
-import { CardDescription } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import ForgotPasswordCard from "@/templates/ForgotPasswordCard";
 
 export default function ForgotPassword() {
-  return (
-    <AccountCard
-      title="Recuperar senha"
-      description="Confirme os dados para recuperar a senha."
-      footer={<Footer />}
-    >
-      <form>
-        <div className="grid w-full items-center gap-4">
-          <div className="flex flex-col space-y-1.5">
-            <Label htmlFor="code">Matrícula</Label>
-            <Input
-              className="remove-arrow"
-              id="code"
-              type="number"
-              placeholder="Digite a matrícula do SIGAA"
-            />
-          </div>
-          <div className="flex flex-col space-y-1.5">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" placeholder="Digite o email institucional" />
-          </div>
-        </div>
-      </form>
-      <div className="mt-4">
-        <CardDescription>
-          Já tem uma conta?{" "}
-          <a className="underline" href="/login">
-            Login
-          </a>
-        </CardDescription>
-      </div>
-    </AccountCard>
-  );
+  return <ForgotPasswordCard />;
 }
-
-const Footer = () => (
-  <>
-    <Button variant="outline">Tentar outra maneira</Button>
-    <Button>Próximo</Button>
-  </>
-);
--- src/templates/ForgotPasswordCard.tsx
+++ src/templates/ForgotPasswordCard.tsx
@@ -13,23 +13,18 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export default function SigninCard() {
+export default function ForgotPasswordCard() {
   return (
     <Card className="max-w-[400px] min-w-[272px] m-auto">
       <CardHeader>
-        <CardTitle>Criar conta</CardTitle>
+        <CardTitle>Recuperar senha</CardTitle>
         <CardDescription>
-          Crie uma conta para fazer os agendamentos.
+          Confirme os dados para recuperar a senha.
         </CardDescription>
       </CardHeader>
       <CardContent>
         <form>
           <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Nome</Label>
-              <Input id="name" placeholder="Digite seu nome" />
-            </div>
-
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="code">Matrícula</Label>
               <Input
@@ -44,23 +39,6 @@ export default function SigninCard() {
               <Label htmlFor="email">Email</Label>
               <Input id="email" placeholder="Digite o email institucional" />
             </div>
-
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="password">Senha</Label>
-              <div className="relative">
-                <PasswordInput id="password" placeholder="Digite sua senha" />
-              </div>
-            </div>
-
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="retype-password">Senha</Label>
-              <div className="relative">
-                <PasswordInput
-                  id="retype-password"
-                  placeholder="Confirme sua senha"
-                />
-              </div>
-            </div>
           </div>
         </form>
         <div className="mt-4">
@@ -73,7 +51,8 @@ export default function SigninCard() {
         </div>
       </CardContent>
       <CardFooter className="flex justify-between">
-        <Button>Cadastar</Button>
+        <Button variant="outline">Tentar outra maneira</Button>
+        <Button>Próximo</Button>
       </CardFooter>
     </Card>
   );
